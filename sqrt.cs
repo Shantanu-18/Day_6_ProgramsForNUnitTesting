0b9e6252e72677160c73fc45f2f40833e6c582c4
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-12 00:51:04 +0530

    [Shantanu] Added: static function to compute the square root

---
+++ sqrt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramsForNUnitTesting
+{
+    class sqrt
+    {
+        public static void sqaureRoot()
+        {
+            Console.Write("Enter number: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double t = c;
+             
+            double epsilon = 1e-15;
+         
+
+            while ( Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = ((c / t) + t) / 2;
+                
+            }
+            
+            Console.WriteLine("Square Root of {0} is {1}",c,t);
+        }
+        
+       
+    }
+}
