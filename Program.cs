8fb4ff663c7758eb5c1a86df3b75d538a1f5f612
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-12 00:42:55 +0530

    [Shantanu] Added: Main Program file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgramsForNUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dayOfWeek.daysOfWeek();
+            //temperaturConversion.temperatureConversion();
+            //vendingMachine.VendingMachine();
+            //monthlyPayment.monthlyPayments();
+            //sqrt.sqaureRoot();
+            toBinary.DecimalToBinary();
+            Console.ReadKey();
+        }
+    }
+}
